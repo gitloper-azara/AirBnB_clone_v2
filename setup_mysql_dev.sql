b872d95bf95de0a5b5aad5ad9eeec8dad85504b3
Azara <yyushahu02@gmail.com>
2024-03-21 05:56:02 +0000

    setup db script

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,15 @@
+-- Setup a MySQL server
+
+-- Create the database
+CREATE DATABASE IF NOT EXISTS 'hbnb_dev_db';
+
+-- create user
+CREATE USER IF NOT EXISTS 'hbnh_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- grant necessary priviledges
+GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON 'performace_schema'.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON 'hbnb_dev_db'.* TO 'hbnb_dev'@'localhost';
+
+-- flush privildges
+FLUSH PRIVILEGES;

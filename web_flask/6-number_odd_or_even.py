235dee87b241a66f2b820023b75ba5dccfee4a21
Azara <yyushahu02@gmail.com>
2024-04-18 22:57:18 +0000

    check parity of numbers if they are integers, else 404

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+'''A python script that starts a Flask web application'''
+from flask import Flask, render_template
+
+# create Flask application instance
+app = Flask(__name__)
+
+# route for the root URL
+@app.route('/', strict_slashes=False)
+def hello_HBNB():
+    '''Displays 'Hello HBNB!'
+    '''
+    return 'Hello HBNB!'
+
+# route for /hbnb
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    '''Displays 'HBNB'
+    '''
+    return 'HBNB'
+
+# route for /c/<text>
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    '''Displays a text!'''
+    text = text.replace('_', ' ')
+    return f'C {text}'
+
+# route for /python/<text>
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text='is cool'):
+    '''Displays a text!'''
+    text = text.replace('_', ' ')
+    return f'Python {text}'
+
+# route for /number/<n>
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_route(n):
+    '''displays a number if number is an int'''
+    return f'{n} is a number'
+
+# route for /number_template/<n>
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template_route(n):
+    '''displays a number based on a template'''
+    return render_template('5-number.html', n=n)
+
+# route for /number_odd_or_even/<n>
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_even(n):
+    '''shows if number is odd or even using a template'''
+    # check odd or even parity
+    parity = 'even' if n % 2 == 0 else 'odd'
+    return render_template('6-number_odd_or_even.html', n=n, parity=parity)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
